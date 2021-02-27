c95217b76eb01e8c15e347172ea08be9f7f8ae40
Lucas Balieiro <lucasbalieirom@hotmail.com>
2021-02-26 22:10:43 -0400

    Added Global Exception Handling

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
+/* eslint-disable no-console */
 import 'reflect-metadata';
 import './database';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import routes from './routes';
+import AppError from './errors/AppError';
 
 const app = express();
 
@@ -14,7 +17,20 @@ app.get('/', (request, response) => {
   response.json({ message: 'Welcome to conexia API' });
 });
 
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.httpStatusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+  console.error(err);
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
+});
+
 app.listen(process.env.PORT || 3333, () => {
-  // eslint-disable-next-line no-console
   console.info('Server started');
 });
