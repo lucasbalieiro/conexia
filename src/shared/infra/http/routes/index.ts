1c174e5e22aa7ca9cecfd382507fc5460ae497b5
Lucas Balieiro <lucasbalieirom@hotmail.com>
2021-02-28 19:34:11 -0400

    ROutes and migrations for forgot/reset password

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/passwords', passwordRouter);
 
 export default routes;
