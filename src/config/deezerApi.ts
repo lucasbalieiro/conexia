63eac4b9589d88dbb1402634879c03a6b9b3e34a
Lucas Balieiro <lucasbalieirom@hotmail.com>
2021-03-01 01:49:13 -0400

    .

--- src/config/deezerApi.ts
+++ src/config/deezerApi.ts
@@ -1,3 +1,3 @@
 export default {
-    secretKey: 'ec3ae4b15441bf7de5c1dc32d7f38247'
+    secretKey: '-'
 }
--- src/modules/users/providers/MusicProvider/implementations/DeezerMusicProvider.ts
+++ src/modules/users/providers/MusicProvider/implementations/DeezerMusicProvider.ts
@@ -1,10 +1,9 @@
 import IMusicRecomendationDTO from "../dtos/IMusicRecomendationDTO";
 import IMusicProvider from "../models/IMusicProvider";
-import axios from 'axios'
 
 export default class DeezerMusicProvider implements IMusicProvider {
-    public async getRecomendationBasedOnTemperature(temperature: number): Promise<IMusicRecomendationDTO[]> {]
-        const client = axios.get('https://developers.deezer.com/api/explorer?url=search/radio?q=Pop')
+    public async getRecomendationBasedOnTemperature(temperature: number): Promise<IMusicRecomendationDTO[]> {
+        
         return [{
             album: 'Test Album',
             artists: 'Test Musician',
